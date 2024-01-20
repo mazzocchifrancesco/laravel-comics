c6c3f20b9239cc41b70c413b3093af0d827eb70c
FrancescoMazzocchi <123@email.it>
2024-01-20 20:02:46 +0100

    add full footer

--- config/data.php
+++ config/data.php
@@ -179,6 +179,25 @@ return [
                 "img" => "buy-dc-power-visa.svg"
             ],
         ],
+        "footerLinks" => [
+            [
+                "title" => "dc comics",
+                "links" => ["characters", "comics", "movies", "TV", "games", "videos", "news"]
+            ],
+            [
+                "title" => "shop",
+                "links" => ["shop DC", "shop DC collectibles"]
+            ],
+            [
+                "title" => "dc",
+                "links" => ["term of use", "privacy policy (new)", "ad choices", "advertising", "jobs", "subscription", "talent workshops", "CPSC certificates", "ratings", "shop help", "contact us"]
+            ],
+            [
+                "title" => "sites",
+                "links" => ["DC", "MAD magazine", "DC kids", "DC universe", "DC power visa"]
+            ],
+
+        ]
     ],
     "common" => []
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/css/bootstrap.rtl.min.css" integrity="sha512-rXhL8a+X3wTosWn2Zgd/5L8rfWrpff7qdOnl7Wg1n2zHk8lHGhiSujoyxoKw1nf43kZAzJoe0Z0ymr2Kkku7lQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="/assets/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="/Assets/css/style.css">
 </head>
 
