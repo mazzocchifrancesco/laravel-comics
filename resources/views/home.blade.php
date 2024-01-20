d91184cd0babcb69f312f58eaf39c39aec8e2d0e
FrancescoMazzocchi <123@email.it>
2024-01-20 14:55:46 +0100

    add cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,30 @@
 
 @section('content')
 
-@foreach ($comics as $fumetto)
 
-<p>{{$fumetto['title']}}</p>
-        @endforeach
+
+
+<div id="main">
+
+    <div id="jumbo"></div>
+    <div id="cardBox" class="position-relative">
+        <div id="series" class="position-absolute bg-primary px-3 fs-5 top-0 translate-middle">CURRENT SERIES
+        </div>
+        <div id="cardBox" class="container">
+            <div class="row">
+            @foreach ($comics as $fumetto)
+                <div class="col-2" id="card">
+                    <img class="thumb" src={{$fumetto['thumb']}} alt="">
+                    <p class="text-uppercase">{{ $fumetto['title'] }}</p>
+                </div>
+                @endforeach
+            </div>
+        </div>
+        <div class="d-flex justify-content-center">
+            <div class="btn btn-primary rounded-0 px-4"> LOAD MORE</div>
+        </div>
+    </div>
+</div>
+
 
         @endsection
